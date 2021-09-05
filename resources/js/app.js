7903f904f06ddf24f6ff196423bd89d804631c06
kazuki0517 <kh2328pkh@gmail.com>
2021-09-05 13:46:21 +0900

    feat component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,10 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
+
+Vue.component('header-component', HeaderComponent);
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,6 +14,19 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,4 +49,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
